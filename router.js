b433c842fec6fd970455f1571cb734405f70479f
Rajadhi Wickramatunga <rwickramatunga@aconex.com>
2012-06-04 16:24:02 +1000

    Added support to handle simple POST data

--- router.js
+++ router.js
@@ -1,8 +1,8 @@
-function route(handle, pathname, response) {
+function route(handle, pathname, response, postData) {
     console.log("ROUTER: About to route a request for " + pathname);
     
     if(typeof handle[pathname] == 'function') {
-        handle[pathname](response);
+        handle[pathname](response, postData);
     } else {
         console.log("No request handler found for " + pathname);
         response.writeHead(404, {"Content-type": "text/plain"});
