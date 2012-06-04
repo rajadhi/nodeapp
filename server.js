6d7aa5494d618615e531cdaafdbb2e482b87f043
Rajadhi Wickramatunga <rwickramatunga@aconex.com>
2012-06-04 15:38:32 +1000

    Added requestHandlers

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 var http = require("http");
+var url = require("url");
 
-function start() {
+function start(route, handle) {
     function onRequest(request, response) {
-        console.log("Request received.");
+        var pathname = url.parse(request.url).pathname;
+        console.log("Request for " + pathname + " received.");
+        
         response.writeHead(200, {"Content-type": "text/plain"});
-        response.write("Hello world");
+        
+        var content = route(handle, pathname);
+        response.write(content);
         response.end();
     }
 
